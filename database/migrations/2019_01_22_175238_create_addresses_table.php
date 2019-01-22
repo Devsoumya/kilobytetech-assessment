ff6abcceabfc5edc99bc502ce15e6c43f207f138
Devsoumya <soumya.manchanda19@gmail.com>
2019-01-22 23:38:32 +0530

    Data Migrations added

---
+++ database/migrations/2019_01_22_175238_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('storeName')->unique();
+            $table->string('area');
+            $table->string('city');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
---
+++ database/migrations/2019_01_22_175455_create_catalogues_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCataloguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catalogues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('category_id');
+            $table->unsignedInteger('address_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('address_id')->references('id')->on('addresses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catalogues');
+    }
+}
