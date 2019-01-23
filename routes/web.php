9d9e3c5b0558bee9ae4189433899558b02899551
Devsoumya <soumya.manchanda19@gmail.com>
2019-01-23 09:13:41 +0530

    Login and JWT Authentication

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post(
+    'auth/login',
+    [
+        'uses' => 'AuthController@authenticate'
+    ]
+);
+
+
+$router->group(
+    ['middleware' => 'jwt.auth'],
+    function() use ($router) {
+        /*
+         * JWT Auth Protected Routes here
+         */
+
+    }
+);
